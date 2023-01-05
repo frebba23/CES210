6aa694c7d0d2f5aa6bb731a20a0207a78b31f19d
frebba23 <frebba2@gmail.com>
2023-01-05 20:17:01 +0200

    prep1 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string surname = Console.ReadLine();
+        Console.WriteLine($"My name is {surname}, {name} {surname}");
     }
 }
